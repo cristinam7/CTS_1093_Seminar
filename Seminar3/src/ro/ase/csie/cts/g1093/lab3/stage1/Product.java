426576aa36ded40a0db01a73dfbde034672a9c2c
cristinam7 <cmironn@gmail.com>
2021-03-11 14:37:09 +0200

    Before the break

--- Seminar3/src/ro/ase/csie/cts/g1093/lab3/stage1/Product.java
+++ Seminar3/src/ro/ase/csie/cts/g1093/lab3/stage1/Product.java
@@ -2,11 +2,12 @@ package ro.ase.csie.cts.g1093.lab3.stage1;
 
 public class Product {
 	
-	public static final int MAX_ACCOUNT_AGE = 10; 
+	public final static int MAX_ACCOUNT_AGE = 10; 
+	public final static float MAX_FIDELITY_DISCOUNT = 0.15f; 
 
 	public float getFinalPrice(int producType, float initialPrice, int accountAgeInYears) {
 		float finalPrice = 0;
-		float fidelityDiscount = (accountAgeInYears > 10) ? (float) 15 / 100 : (float) accountAgeInYears / 100;
+		float fidelityDiscount = (accountAgeInYears > MAX_ACCOUNT_AGE) ? MAX_FIDELITY_DISCOUNT : (float) accountAgeInYears / 100;
 		if (producType == 1) {
 			finalPrice = initialPrice;
 		} else if (producType == 2) {
